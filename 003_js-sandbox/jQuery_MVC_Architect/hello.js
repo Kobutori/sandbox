19cd5aebfa82d9b269551130b923823b186c3073
kobutori <kobutorina.hato@gmail.com>
2019-08-01 02:22:21 +0900

    機能ごとに分解

--- 003_js-sandbox/jQuery_MVC_Architect/hello.js
+++ 003_js-sandbox/jQuery_MVC_Architect/hello.js
@@ -1,38 +1,62 @@
 "use strict"; //落とし穴になりやすい機能の制限
 
-//メッセージを表示するイベントハンドラ
-function doShowMessage(){
-    let name = $('#name').val();
-    name = name.trim();
-    if(! name){
-        alert("Enter your name.");
-    }else{
-        let msg = "Hello." + name + "!";
-        $('#message').text(msg);
-    }
+//=========コア機能=========
+
+//メッセージを作成する
+function buildMessage(name){
+    return "Hello," + name + "!";
 }
-//名前欄とメッセージ欄をクリアするイベントハンドラ
-function doClear(){
+
+//=========表示に関する機能=========
+
+//メッセージを表示する
+function showMessage(msg){
+    $('#message').text(msg);
+}
+
+//アラートの表示
+function showAlert(msg){
+    alert(msg);
+}
+
+//名前欄とメッセージ欄をクリア
+function clear(){
     $('#name').val("");
     $('#message').text("");
 }
 
+//=========ユーティリティ関数=========
+//文字列の前後の空白を取り除く
+function trimString(str){
+    return str.trim();
+}
+
+//=========イベントハンドラ=========
+//「表示」ボタンがクリックされたときのイベントハンドラ
+function doShowMessage(){
+    let name = $('#name').val();
+    name = trimString(name);
+    if(! name){
+        showAlert("Enter your name.");
+    }else{
+        let msg = buildMessage(name);
+        showMessage(msg);
+    }
+}
+
+//「クリア」ボタンをがクリックされたときのイベントハンドラ
+function doClear(){
+    clear();
+}
+
 //ボタンにイベントハンドラを登録する
-$(function(){
+function registerHandLers(){
     $('#show-btn').on('click', doShowMessage);
     $('#clear-btn').on('click', doClear);
+}
+
+$(function(){
+    registerHandLers();
 });
 
 
-
-$(function(){//HTMLの読み込みが完了したら下記実行
-    //「表示」ボタンがクリックされたときの処理を登録
-    $('#show-btn').on('click', function(){
-
-    });
-
-    //「クリア」ボタンがlkウリックされたときの処理を登録
-    $('#clear-btn').on('click', function(){
-
-    });
-});
