cfd84facf97fd900fbae7a2e1ef81db2229a8f59
kobutori <kobutorina.hato@gmail.com>
2019-08-01 01:53:20 +0900

    イベントハンドラの関数を定義

--- 003_js-sandbox/jQuery_MVC_Architect/hello.js
+++ 003_js-sandbox/jQuery_MVC_Architect/hello.js
@@ -1,21 +1,38 @@
 "use strict"; //落とし穴になりやすい機能の制限
 
+//メッセージを表示するイベントハンドラ
+function doShowMessage(){
+    let name = $('#name').val();
+    name = name.trim();
+    if(! name){
+        alert("Enter your name.");
+    }else{
+        let msg = "Hello." + name + "!";
+        $('#message').text(msg);
+    }
+}
+//名前欄とメッセージ欄をクリアするイベントハンドラ
+function doClear(){
+    $('#name').val("");
+    $('#message').text("");
+}
+
+//ボタンにイベントハンドラを登録する
+$(function(){
+    $('#show-btn').on('click', doShowMessage);
+    $('#clear-btn').on('click', doClear);
+});
+
+
+
 $(function(){//HTMLの読み込みが完了したら下記実行
     //「表示」ボタンがクリックされたときの処理を登録
     $('#show-btn').on('click', function(){
-        let name = $('#name').val();
-        name = name.trim();
-        if(! name){
-            alert("Enter your name.");
-        }else{
-            let msg = "Hello." + name + "!";
-            $('#message').text(msg);
-        }
+
     });
 
     //「クリア」ボタンがlkウリックされたときの処理を登録
     $('#clear-btn').on('click', function(){
-        $('#name').val("");
-        $('#message').text("");
+
     });
 });
