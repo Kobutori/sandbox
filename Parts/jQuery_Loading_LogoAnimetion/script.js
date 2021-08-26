fb9aeb28863679b3bbffe5ffcd7ee69f9156cce2
kobutori <kobutorina.hato@gmail.com>
2021-08-27 03:37:54 +0900

    ディレクトリ変更・common.css変更

--- Parts/jQuery_Loading_LogoAnimetion/script.js
+++ Parts/jQuery_Loading_LogoAnimetion/script.js
@@ -1,5 +1,5 @@
 // Logoの表示
-$(window).on('load' function(){
+$(window).on('load',function(){
   //ローディング画面を1.5秒(1500ms)待機してからフェードアウト
   $("#splash").delay(1500).fadeOut('slow');
   //ロゴを1.2秒待機してからフェードアウト
